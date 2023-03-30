b65e35e2d61be79ec4541a187bf303d5c286014b
Caleonar <97125703+Caleonar@users.noreply.github.com>
2023-03-30 16:20:31 +0200

    Delete makefile.sh
    Delete per sostituzione tramito comando make

--- makefile.sh
+++
@@ -1,8 +0,0 @@
-.PHONY: all clean
-
-all: README.md
-
-README.md: guessinggame.sh
-	echo "# Guessing Game Project" > README.md
-	echo "Make was run on: `date`" >> README.md
-	echo "The number of lines of code contained in guessinggame.sh is: `wc -l guessinggame.sh | egrep -o "[0-9]+"`" >> README.md
