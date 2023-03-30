bf09be2e78c8fb0d71735700e70df365f2eb4b8a
caleonar <carlalbertoleonardi@gmail.com>
2023-03-30 16:24:56 +0200

    aggiornata shell e relativo README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Guessing Game Project
-Make was run on: mer 29 mar 2023, 22:24:18, CEST
-The number of lines of code contained in guessinggame.sh is: 28
+Make was run on: gio 30 mar 2023, 16:23:52, CEST
+The number of lines of code contained in guessinggame.sh is: 25
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,28 +1,25 @@
-# Definisci la funzione che verifica se l'input dell'utente è un intero
+#!/bin/bash
+
 function is_valid_input() {
-  # Controlla se l'input dell'utente è un intero maggiore di 0
-  if [[ "$1" =~ ^[0-9]+$ && "$1" -gt 0 ]]; then
+  if [[ "$1" =~ ^[0-9]+$ && "$1" -ge 0 ]]; then
     return 0
   fi
   return 1
 }
 
-# Ottieni il numero di file nella directory corrente
-num_files=$(ls -l | grep "^-" | wc -l)
+num_files=$(ls -1a | wc -l)
 
-# Chiedi all'utente di indovinare il numero di file nella directory corrente
 echo "Indovina il numero di file nella directory corrente:"
 read user_guess
-
-# Continua a chiedere all'utente di indovinare fino a quando non indovina il numero corretto
 while ! is_valid_input $user_guess || [[ "$user_guess" -ne "$num_files" ]]; do
-  if [[ "$user_guess" -lt "$num_files" ]]; then
+  if ! is_valid_input "$user_guess"; then
+    echo "Devi inserire un numero! Riprova:"
+  elif [[ "$user_guess" -lt "$num_files" ]]; then
     echo "Troppo basso. Riprova:"
   else
     echo "Troppo alto. Riprova:"
-  fi
+fi
   read user_guess
 done
 
-# L'utente ha indovinato il numero corretto
 echo "Congratulazioni, hai indovinato il numero di file nella directory corrente!"
